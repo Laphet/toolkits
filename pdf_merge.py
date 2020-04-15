79f4db7032687b663be81281d6e501b92bc6a9a9
Laphet <ye_changqing@outlook.com>
2020-04-15 16:20:39 +0800

    Update pdf_merge.py

--- pdf_merge.py
+++ pdf_merge.py
@@ -8,7 +8,10 @@ pdfname_dict = dict(zip(list(string.ascii_letters), pdfname_list))
 for key, name in pdfname_dict.items():
     print("{0}:\t{1}".format(key, name))
 
-input_str = input("Set the order of PDF files:")
+input_str = input("Set the order of the PDF files (default abcdefg...):")
+if (len(input_str) == 0):
+    input_str = ''.join(pdfname_dict.keys())
+
 pdfs = []
 for key in list(input_str):
     pdfs.append(pdfname_dict.get(key))
